f237d21ff9bf8eaef1fac059c2ef0a7de76679e8
psliwinski <pavelvinski@gmail.com>
2023-09-17 21:26:32 +0200

    Implement functionalities and design MainWindow

--- Autocomp.Nmea.Parser/MainWindow.xaml.cs
+++ Autocomp.Nmea.Parser/MainWindow.xaml.cs
@@ -17,7 +17,56 @@ namespace Autocomp.Nmea.Parser
             InitializeComponent();
         }
 
+        private void ParseButton_Click(object sender, RoutedEventArgs e)
+        {
+            ParsedValuesListView.Items.Clear();
+            ErrorTextBlock.Text = string.Empty;
 
+            try
+            {
+                string nmeaSentence = NmeaSentenceTextBox.Text;
+                char separator = DetermineSeparator(nmeaSentence);
+
+                NmeaMessage nmeaMessage = NmeaParser.Parse(nmeaSentence, separator);
+
+                if (nmeaMessage != null)
+                {
+                    
+                    foreach (string fieldValue in nmeaMessage.Fields)
+                    {
+                        ListViewItem fieldItem = new ListViewItem();
+                        fieldItem.Content = fieldValue;
+                        ParsedValuesListView.Items.Add(fieldItem);
+                        
+                    }
+
+                    if (!string.IsNullOrEmpty(nmeaMessage.Checksum))
+                    {
+                        ListViewItem checksumItem = new ListViewItem();
+                        checksumItem.Content = nmeaMessage.Checksum;
+                        ParsedValuesListView.Items.Add(checksumItem);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorTextBlock.Text = $"Error parsing NMEA sentence: {ex.Message}";
+            }
+        }
+
+        private char DetermineSeparator(string nmeaSentence)
+        {
+            char[] possibleSeparators = { ',', ';', ':' }; 
+            foreach (char separator in possibleSeparators)
+            {
+                if (nmeaSentence.Contains(separator))
+                {
+                    return separator;
+                }
+            }
+            
+            throw new FormatException("Unable to determine the separator in the NMEA sentence.");
+        }
     }
 }
 
